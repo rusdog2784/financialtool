a077eb7306f25b3d3578d490690bf391d6d39fbb
Scott Russell <scott.tkdmaster@mac.com>
2016-12-10 16:41:22 -0500

    Initial Commit
    Just set up the Xcode project.

---
+++ Financial Tool/Financial Tool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Financial Tool
+//
+//  Created by Jarvis on 12/10/16.
+//  Copyright © 2016 Rusdoge Industries. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Financial Tool/Financial ToolTests/Financial_ToolTests.swift
@@ -0,0 +1,36 @@
+//
+//  Financial_ToolTests.swift
+//  Financial ToolTests
+//
+//  Created by Jarvis on 12/10/16.
+//  Copyright © 2016 Rusdoge Industries. All rights reserved.
+//
+
+import XCTest
+@testable import Financial_Tool
+
+class Financial_ToolTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
